a50b321cacf46ad2f51f7524cb3226a78593bece
unknown <nilton.oliveira0305@gmail.com>
2023-06-27 23:33:22 -0300

    correção nas legendas das estrelas

--- src/app/(public)/[postId]/page.tsx
+++ src/app/(public)/[postId]/page.tsx
@@ -66,23 +66,25 @@ export default async function Page({ params }: props) {
                 {
                     note.length !== 0 && (
                         <div>
-                            <hr />
-                            <div className="flex items-center text-[22px] font-bold mt-3 gap-2 uppercase">
-                                Nota do crítica
-                                <div className="flex">
+                            <hr className="mb-3" />
+                            <div className="flex items-end gap-2">
+                                <div className="flex items-center text-[22px] font-bold gap-2 uppercase">
+                                    <div>
+                                        Nota do crítica
+                                    </div>
                                     {note?.[0].note && [1, 2, 3, 4, 5].map((value) => (
                                         <FaStar
                                             key={value}
                                             className={`${value <= Number(note?.[0].note) ? 'text-yellow-400' : 'text-gray-400'}`}
                                         />
                                     ))}
-                                    <div className="text-[16px]">
-                                        {Number(note?.[0].note) === 1 && 'Horrível'}
-                                        {Number(note?.[0].note) === 2 && 'Ruim'}
-                                        {Number(note?.[0].note) === 3 && 'Bom'}
-                                        {Number(note?.[0].note) === 4 && 'Otimo'}
-                                        {Number(note?.[0].note) === 5 && 'Excelente'}
-                                    </div>
+                                </div>
+                                <div className="text-[16px]">
+                                    {Number(note?.[0].note) === 1 && 'Horrível'}
+                                    {Number(note?.[0].note) === 2 && 'Ruim'}
+                                    {Number(note?.[0].note) === 3 && 'Bom'}
+                                    {Number(note?.[0].note) === 4 && 'Otimo'}
+                                    {Number(note?.[0].note) === 5 && 'Excelente'}
                                 </div>
                             </div>
                             <hr className="mt-3" />
