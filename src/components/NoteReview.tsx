f6c43decdd45d74f089f8c130fde10e8cdbafecb
unknown <nilton.oliveira0305@gmail.com>
2023-07-15 13:12:53 -0300

    correção das estrelas

--- src/components/NoteReview.tsx
+++ src/components/NoteReview.tsx
@@ -6,19 +6,28 @@ type props = {
     }[]
 }
 export function NoteReview({ note }: props) {
-    return <div className="flex items-center mt-3">
-        {note?.[0]?.note && [1, 2, 3, 4, 5].map((value) => (
-            <FaStar
-                key={value}
-                className={`${value <= Number(note?.[0].note) ? 'text-yellow-400' : 'text-gray-400'}`}
-            />
-        ))}
-        <div className="text-[16px]">
-            {Number(note?.[0].note) === 1 && 'Ruim'}
-            {Number(note?.[0].note) === 2 && 'Regular'}
-            {Number(note?.[0].note) === 3 && 'Bom'}
-            {Number(note?.[0].note) === 4 && 'Ótimo'}
-            {Number(note?.[0].note) === 5 && 'Excelente'}
+    return <div>
+        <hr className="mb-3" />
+        <div className="flex items-end gap-2">
+            <div className="flex items-center text-[22px] font-bold gap-2 uppercase">
+                <div>
+                    Nota do crítica
+                </div>
+                {note?.[0].note && [1, 2, 3, 4, 5].map((value) => (
+                    <FaStar
+                        key={value}
+                        className={`${value <= Number(note?.[0].note) ? 'text-yellow-400' : 'text-gray-400'}`}
+                    />
+                ))}
+            </div>
+            <div className="text-[16px]">
+                {Number(note?.[0].note) === 1 && 'Horrível'}
+                {Number(note?.[0].note) === 2 && 'Ruim'}
+                {Number(note?.[0].note) === 3 && 'Bom'}
+                {Number(note?.[0].note) === 4 && 'Otimo'}
+                {Number(note?.[0].note) === 5 && 'Excelente'}
+            </div>
         </div>
+        <hr className="mt-3" />
     </div>
 }
